33041395a236fe2ce84c0ab567ed6c90f673b363
Vaani <vaanipathariya@gmail.com>
2024-05-14 22:29:30 +0530

    Adding changes

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ io.on("connection", (socket) => {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-      (user.latitude = data.latitude), (user.longitude = data.longitude) , (user.locationUpdate= new Date());
+      user.latitude = data.latitude;
+      user.longitude = data.longitude;
+      user.locationUpdate = new Date();
       user.save();
       console.log("Location saved")
     } catch (error) {
